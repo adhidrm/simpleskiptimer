c0e19d118bec09e73eb6d1e3c4b5b355fd1ea192
Adhi <darkseekertm@gmail.com>
2021-10-12 17:08:22 +0700

    Create skip.js

---
+++ skip.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         GBFIle Skipper
+// @namespace    https://adhidrm.me/
+// @version      0.1
+// @author       adhidrm
+// @match        http*://gbfile.*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.seconds = -1;
+})();
